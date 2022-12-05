afe855a5e2ba9eafa4dd2e29e16191230a7281d6
Urvisha1303 <bambhroliyaurvisha79@gmail.com>
2022-12-05 15:53:23 +0530

    lec3

--- lec3.js
+++ lec3.js
@@ -50,3 +50,50 @@ let var5 = "python"
 let var6 = "jargon"
 console.log(!(var5.includes('on') && var6.includes('on')))
 
+//Data Object
+
+const now = new Date()
+console.log(now)
+console.log(now.getMonth() +1)   // return month 1-11
+console.log(now.getDate())
+console.log(now.getDay())  // sunday- 0 , monday-1
+console.log(now.getHours())
+console.log(now.getMinutes())
+
+// const secondsSinceEpoch = Math.round(now.getTime() / 1000)
+// console.log(secondsSinceEpoch)
+
+//Exercises : Level 2 
+
+// const basevalue = prompt("Enter Base Value:")
+// const heightvalue  = prompt("Enter Height value:")
+// const area = ((basevalue * heightvalue) / 2)
+// console.log(`The area of the triangle is ${area}`)
+
+// const A = parseInt(prompt('Enter Side a:'));
+// const B = parseInt(prompt('Enter Side b:'));
+// const C = parseInt(prompt('Enter Side C:'));
+// const perimeter = (A + B + C);
+// console.log(`The Perimeter of the triangle ${perimeter}`)
+
+// const length = parseInt(prompt("Enter Length:"))
+// const width = parseInt(prompt("Enter width:"))
+// const perimeter = 2 * (length + width)
+// console.log(perimeter)
+
+// const r = prompt("Enter The value Of radius:")
+// const area = parseInt(Math.PI * (r * r)) 
+// const circle = parseInt(Math.PI * 2 * r)
+// console.log(area)
+// console.log(circle)
+
+// const x = prompt("Enter X Value:")
+// const y = prompt("Enter Y Value:")
+// const both = 2*x -2
+// console.log(both)
+
+
+// const hours = prompt("Enter Hours:")
+// const perhoursrate = prompt("Enter Rate Per Hour:")
+// const person = (hours * perhoursrate)
+// console.log(person)
