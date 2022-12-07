2e5ff8f67dd3595396a99ffb9f7ab3a2f1c20d2c
Urvisha1303 <bambhroliyaurvisha79@gmail.com>
2022-12-07 18:22:11 +0530

    lec4

---
+++ lec4.js
@@ -0,0 +1,92 @@
+//Exercises :Level 1 
+
+// const age = prompt("Enter Your Age :")
+// if (age > 18) {
+//     console.log("You are old enough to drive.")
+// } else {
+//     console.log (`You are left with ${18- age} years to drive.`)
+// }
+
+
+// let myage = 25
+// const yourage = prompt("Enter Your age:")
+// if (myage > yourage) {
+//     console.log(`You are ${yourage-myage} years older than me.`)
+// }else {
+//     console.log(`You are ${yourage-myage} years elder than me.`)
+// }
+
+// 4.1 let a = 4
+// let b = 3
+// if (a > b){
+//     console.log("a is greater than b")
+// }else {
+//     console.log("a is less than b")
+// }
+
+//3.2 let a = 4
+// let b = 3
+// a > b ? console.log("a is greater than b") : console.log("a is less than b")
+
+
+// const number = prompt("Enter a Number:")
+// if (number % 2 === 0){
+//     console.log(`${number} is a even number.`)
+// }else{
+//     console.log(`${number} is an odd number.`)
+// }
+
+//Exercises :Level 2 
+
+// const marks = prompt("Enter the student marks:")
+// if (marks <= 100 && marks >= 80){
+//     console.log("Grade : A")
+// }else if (marks <= 89 && marks >= 70) {
+//     console.log("Grade : B")
+// }else if (marks <= 69 && marks >= 60) {
+//     console.log("Grade : C")   
+// }else if (marks <= 59 && marks >= 50) {
+//     console.log("Grade : D")   
+// }else{
+//     console.log("Grade : F")
+// }
+
+
+// const month = prompt("Enter the month:")
+// if(month==="September" || month==="Octomber" || month==="November"){
+//     console.log("This season is Autumn")
+// }else if(month==="December"|| month==="January" || month==="february"){
+//     console.log("Winter")
+// }else if(month==="March" || month==="April" || month==="May"){
+//     console.log("Spring")
+// }else{
+//     console.log("Summer")
+// }
+
+
+// let dayUserInput = prompt('What day is today ?')
+// switch (dayUserInput) {
+//     case 'monday':
+//       console.log('Monday is a working day.')
+//       break
+//     case 'tuesday':
+//       console.log('Tuesday is a working day.')
+//       break
+//     case 'wednesday':
+//       console.log('Wednesday is a working day.')
+//       break
+//     case 'thursday':
+//       console.log('Thursday is a working day.')
+//       break
+//     case 'friday':
+//       console.log('Friday is a working day.')
+//       break
+//     case 'saturday':
+//       console.log('Saturday is a Weekend')
+//       break
+//     case 'sunday':
+//       console.log('Today is Sunday')
+//       break
+//     default:
+//       console.log('It is not a week day.')
+// }
