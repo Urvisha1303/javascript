27f72dffd1a89ca411cb05ef8b103c51671becea
Urvisha1303 <bambhroliyaurvisha79@gmail.com>
2022-12-29 08:10:57 +0530

    project

--- lec6.js
+++ lec6.js
@@ -262,12 +262,6 @@ console.log(c(28))
 // }
 
 
-// const i = [1,2,3,4,5,6,7,8,9,10]
-// for (let i = 10; i>=0; i--){
-//     console.log(i)
-// }
-
-
 //15.
 // const  fruit = ['banana', 'orange', 'mango', 'lemon']
 // let newArray = [];
